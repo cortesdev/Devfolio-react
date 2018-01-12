53390b0b957fdd4ace30f15704de71c6bfc8f125
Cortesdev <giralander@hotmail.com>
2018-01-12 19:40:33 +0100

    Icon fonts init

--- src/common/components/layout/Navigation.js
+++ src/common/components/layout/Navigation.js
@@ -10,20 +10,23 @@ class Navigation extends Component {
         <div className="Navigation nav-wrapped navbar-nav">
             <a className="navbar-brand" href="#"> </a>
             <nav className="nav navbar-nav">
-                <Link to="/home" className="navbar-nav-item" onClick={this.eventCloseSidebar} activeClassName="active">Home</Link>
-                <Link to="/portfolio" className="navbar-nav-item" onClick={this.eventCloseSidebar} activeClassName="active">My Portfolio</Link>
-                <Link to="/services" className="navbar-nav-item" onClick={this.eventCloseSidebar} activeClassName="active">My Services</Link>
-                <Link to="/about" className="navbar-nav-item" onClick={this.eventCloseSidebar} activeClassName="active">About</Link>
+                <Link to="/home" className="navbar-nav-item home" onClick={this.eventCloseSidebar} activeClassName="active"><i className="la la-home"></i></Link>
+                <Link to="/portfolio" className="navbar-nav-item portfolio" onClick={this.eventCloseSidebar} activeClassName="active"><i className="la la-folder-open portfolio"></i> </Link>
+                <Link to="/services" className="navbar-nav-item services" onClick={this.eventCloseSidebar} activeClassName="active"><i className="la la-desktop services" ></i></Link>
+                <Link to="/about" className="navbar-nav-item about" onClick={this.eventCloseSidebar} activeClassName="active"><i className="la la-info-circle about"></i></Link>
             </nav>
         </div>
 
         <div className="Navigation Navigation-item Navigation-footer">
             <p>
-                Visit <a href="https://github.com/cortesdev">My GitHub Repo</a><br/>
-                Visit <a href="https://www.linkedin.com/in/cortesdev">My Linkedin</a><br/>
-                Visit <a href="https://twitter.com/cortesdev">My Twitter</a><br/>
+                Visit
+                <br/>
+                  <a href="https://github.com/cortesdev"><i className="la la-gitHub"></i></a><br/>
+                  <a href="https://www.linkedin.com/in/cortesdev"><i className="la la-linkedin"></i></a><br/>
+                  <a href="https://facebook.com/cortesde"><i className="la la-facebook"></i></a><br/>
+
             </p>
-        </div>        
+        </div>
 
     </div>
     );
